0cdc8a1041d6f7ac5f5afb0db71c616ba40187a1
rashaev <rashaev@yandex.ru>
2019-11-24 21:33:15 +0300

    added SIGINT handler

---
+++ Makefile
@@ -0,0 +1,5 @@
+build:
+	go build -o bin/go-telnet src/main.go
+
+clean:
+	@rm -rf bin
--- src/main.go
+++ src/main.go
@@ -4,21 +4,22 @@ package main
 import (
 	"net"
 	"bufio"
-	//"io"
 	"fmt"
 	"os"
-	"log"
+	"os/signal"
 	"context"
 	"time"
 )
 
 
 func connection(ctx context.Context, ip string, chanReq chan string, chanRes chan string) {
+	ctx, _ = context.WithTimeout(ctx, 3 * time.Second)
 	var d net.Dialer
 	fmt.Fprint(os.Stdout, "Trying ", ip, "...\n")
 	conn, err := d.DialContext(ctx, "tcp", ip)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("%s\n", err)
+		os.Exit(1)
 	} else {
 		fmt.Fprint(os.Stdout, "Connected to ", ip, "\n")
 		message, _ := bufio.NewReader(conn).ReadString('\n')
@@ -27,36 +28,40 @@ func connection(ctx context.Context, ip string, chanReq chan string, chanRes cha
 	defer conn.Close()
 
 	for {
-		select {
-		case x := <-chanReq:
-			conn.Write([]byte(x+"\n"))
-			message, _ := bufio.NewReader(conn).ReadString('\n')
-			chanRes<- message
-		//case <-ctx.Done():
-			//return
+		x := <-chanReq
+		conn.Write([]byte(x+"\n"))
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		chanRes<- message
 		}
 	}
 
-}
+
 
 func main() {
 	var requestCh = make(chan string)
 	var responseCh = make(chan string)
+	var signalCh = make(chan os.Signal, 1)
+
+	signal.Notify(signalCh, os.Interrupt)
+	fmt.Println("Press CTRL + C to interrupt the program")
 
 	ctx := context.Background()
-	ctx, _ = context.WithTimeout(ctx, 10000	 * time.Millisecond)
+	
 	go connection(ctx, "smtp.yandex.ru:25", requestCh, responseCh)
 
-	fmt.Println(<-responseCh)
+	fmt.Printf("%s", <-responseCh)
 	scanner := bufio.NewScanner(os.Stdin)
+
 	for {
 		scanner.Scan() 
 		requestCh<- scanner.Text()
-
-		if err := scanner.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, "reading standard input:", err)
-		}
-		fmt.Println(<-responseCh)	
+		
+		select {
+		case servRepl := <-responseCh:
+			fmt.Printf("%s", servRepl)
+		case <-signalCh:
+			os.Exit(0)
+		}	
 	}
 	
 }
